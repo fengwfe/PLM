8b876ab4e6886e6d86f35a796fa58ad977215d88
Fuen Wu <fuen.wu@gmail.com>
2021-05-25 19:41:55 +0800

    add constants

--- src/main/java/com/tnt/plm/constant/AdminConstants.java
+++ src/main/java/com/tnt/plm/constant/AdminConstants.java
@@ -6,4 +6,25 @@ public interface AdminConstants {
     String NODE_ATTR = "1";
 
     String P_LABEL = "0";
+
+
+    // admin client对应的就是这个root的Node
+    String NODE_KEY_ADMIN_ROOT = "ADMIN_ROOT";
+
+    //各种class定义
+    String NODE_KEY_CLASSES = "CLASSES";
+    String NODE_KEY_BASE_CLASS_ITEM = "BASE_CLASS_ITEM";
+    String NODE_KEY_CLASS_PARTS = "CLASS_PARTS";
+    String NODE_KEY_CLASS_DOCUMENTS = "CLASS_DOCUMENTS";
+
+
+    // 各种tab定义
+    String NODE_KEY_TAB_PARTS_GI = "TAB_PARTS_GI";
+    String NODE_KEY_TAB_PARTS_BOM = "TAB_PARTS_BOM";
+    String NODE_KEY_TAB_PARTS_HISTORY = "TAB_PARTS_HISTORY";
+
+    //各种attribute 定义
+    String NODE_KEY_ATTR_PARTS_GI_NUMBER = "ATTR_PARTS_GI_NUMBER";
+    String NODE_KEY_ATTR_PARTS_GI_DESCRIPTION = "ATTR_PARTS_GI_DESCRIPTION";
+
 }
